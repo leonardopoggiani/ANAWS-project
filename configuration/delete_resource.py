a6637b68245e16e0d56408e27b574d35221757b0
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-03-16 17:56:43 +0100

    Update delete_resource.py

--- configuration/delete_resource.py
+++ configuration/delete_resource.py
@@ -17,6 +17,7 @@ while(True):
         print(requests.delete(url_resources, data=json.dumps({"resource": '"' + str(resourceIP) + '"'}), headers=header).json())
     elif str(action) == "2":
         resourceIP = input("Insert the resource virtual ip\n")
+        print(resourceIP)
         userMAC = input("Insert user MAC")
         print(requests.delete(base_url + "/subscribers/" + str(resourceIP) + "/json", data=json.dumps({"MAC": str(userMAC)}), headers=header).json())
     else:
