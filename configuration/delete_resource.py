c58be563a22f451becbf95119153ca6515784ef0
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-03-16 17:51:27 +0100

    Update delete_resource.py

--- configuration/delete_resource.py
+++ configuration/delete_resource.py
@@ -9,14 +9,14 @@ header = {"Content-type": "application/json", "Accept": "text/plain"}
 url_resources = base_url + "resources/json"
 url_subscribers = base_url + "/subscribers/{resource}/json"
 
-while(True)
-    action = input("1) Delete resource\n 2)Unsubscribe user\n ")
+while(True):
+    action = input("1) Delete resource\n2)Unsubscribe user\n ")
 
-    if action == "1":
-        resourceIP = input("Insert the resource virtual ip")
-        print(requests.delete(url_resources, data=json.dumps({"resource": str(resourceIP)}), headers=header).json())
-    else if action == "2":
-        resourceIP = input("Insert the resource virtual ip")
+    if str(action) == "1":
+        resourceIP = input("Insert the resource virtual ip\n")
+        print(requests.delete(url_resources, data=json.dumps({"resource": '"' + str(resourceIP) + '"'}), headers=header).json())
+    else if str(action) == "2":
+        resourceIP = input("Insert the resource virtual ip\n")
         userMAC = input("Insert user MAC")
         print(requests.delete(base_url + "/subscribers/" + str(resourceIP) + "/json", data=json.dumps({"MAC": str(userMAC)}), headers=header).json())
     else:
