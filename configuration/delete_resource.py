fb4cb50c5dd66d151955acbe0f2d9c0f7ddae8b7
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-03-21 10:34:59 +0100

    ..

--- configuration/delete_resource.py
+++ configuration/delete_resource.py
@@ -8,9 +8,10 @@ base_url = "http://" + controller_ip + "/db/"
 header = {"Content-type": "application/json", "Accept": "text/plain"}
 
 url_resources = base_url + "resources/json"
-if len(sys.arg) < 1
+howManyArgument = len(sys.argv)
+if howManyArgument < 1
     print("Insert the ip address of the resource as argument\n")
-elif len(sys.arg) > 1
+elif howManyArgument > 1
     print("Too many arguments, just enter the ip address of the resource\n")
 else
     resourceIP = sys.argv[1]
