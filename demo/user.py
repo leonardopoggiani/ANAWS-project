46c6b1ab4d1aea4687fe40bde02476a1ea55a6fa
Bruno Augusto Casu <45639319+brunocasu@users.noreply.github.com>
2022-03-29 18:59:42 +0200

    updates in user.py - uniform rest server response code

--- demo/user.py
+++ demo/user.py
@@ -32,13 +32,14 @@ class TestUser(object):
             print("max number of resources created")
 
 
-    def delete_resource(self, resourceIP):
+    def delete_resource(self, resource_ip):
         #  access REST interface /resources - DELETE to remove a resource
-        controller_ip = "192.168.122.1:8080"
-        base_url = "http://" + controller_ip + "/db/"
-        header = {"Content-type": "application/json", "Accept": "text/plain"}
-        url_resources = base_url + "resources/json"
-        print(requests.delete(url_resources, data=json.dumps({"resource": '"' + str(resourceIP) + '"'}), headers=header).json())
+        r = requests.delete(self.broker_rest_url_resources, data=json.dumps({"resource": str(resource_ip)}), headers=self.broker_rest_header)
+        if (r.status_code == 200):  # success
+            print("REST server response: ")
+            print(r.text)
+        else: #  failed to get resources
+            print(r.text)
 
   
     def get_resources(self):
@@ -65,7 +66,6 @@ class TestUser(object):
     def get_subscribers (self, resource_ip):
         #  access REST interface /subscribers - GET to retrieve the list of subscribers in a resource
         rest_addr = self.broker_rest_base_url + "/subscribers/" + resource_ip + "/json"
-        #r = requests.get(rest_addr, json.dumps({"IPaddress": sub_ip, "MAC": sub_mac}), headers=self.broker_rest_header)
         r = requests.get(rest_addr, headers=self.broker_rest_header)
         if (r.status_code == 200):  # success
             print("REST server response: ")
@@ -74,13 +74,15 @@ class TestUser(object):
             print(r.text)
 
 
-    def remove_cn(self, resourceIP, userMAC):
+    def remove_cn(self, resource_ip, sub_mac):
         #  access REST interface /subscribers - DELETE to remove the subscriber from a resource
-        controller_ip = "192.168.122.1:8080"
-        base_url = "http://" + controller_ip + "/db/"
-        header = {"Content-type": "application/json", "Accept": "text/plain"}
-        print(requests.delete(base_url + "/subscribers/" + str(resourceIP) + "/json", data=json.dumps({"MAC": str(userMAC)}), headers=header).json())
-        
+        rest_addr = self.broker_rest_base_url + "/subscribers/" + resource_ip + "/json"
+        r = requests.delete(rest_addr, data=json.dumps({"MAC": str(sub_mac)}), headers=self.broker_rest_header)
+        if (r.status_code == 200):  # success
+            print("REST server response: ")
+            print(r.text)
+        else: #  failed to get resources
+            print(r.text)
         
         
 
